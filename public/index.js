91204dbff0fdbd4788816afead7136d663e8eb17
PiotrBerebecki <petebere@gmail.com>
2017-03-06 15:56:15 +0000

    Add tests

--- public/index.js
+++ public/index.js
@@ -1,1 +1,58 @@
-/* let's go! */
+var urlProfile = 'https://api.github.com/users/piotrberebecki';
+var urlRepos = 'https://api.github.com/users/piotrberebecki/repos';
+var langCount;
+var starCount;
+
+function makeRequest(url, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var jsonObj = JSON.parse(xhr.responseText);
+      cb(jsonObj);
+    }
+  }
+  xhr.send();
+}
+
+
+function showUserProfile(data) {
+  imgDOM.src = data.avatar_url;
+  repoNumberDOM.textContent = data.public_repos;
+}
+
+
+function handleRepoResults(reposArr) {
+  // showUserLanguages
+  showUserLanguages(reposArr);
+
+  // showTopRepo
+  showTopRepo(reposArr);
+}
+
+
+function showUserLanguages(data) {
+  langDOM.textContent = getLang(data);
+}
+
+
+function getLang(data) {
+  return data.map(function(repo){
+    return repo.language
+  }).join(' ');
+}
+
+
+function showTopRepo(data) {
+  // show top repo
+}
+
+
+var imgDOM = document.getElementById('github-user-avatar');
+var repoNumberDOM = document.getElementById('github-user-repos');
+var langDOM = document.getElementById('github-repos-languages');
+
+
+makeRequest(urlProfile, showUserProfile);
+makeRequest(urlRepos, handleRepoResults);
--- tests/tests.js
+++ tests/tests.js
@@ -2,4 +2,25 @@ test( "hello test", function( assert ) {
   assert.ok( 1 == "1", "Passed!" );
 });
 
-// add your tests here using QUnit!
+test('does avatar image exist', function(assert){
+  var done = assert.async();
+  setTimeout(function() {
+    assert.equal(imgDOM.src, 'https://avatars1.githubusercontent.com/u/17753038?v=3');
+    done();
+  }, 0);
+
+});
+
+test('is Number of repos returning a number', function(assert){
+  var done = assert.async();
+  setTimeout(function() {
+    assert.equal(typeof Number(repoNumberDOM.textContent), 'number');
+    done();
+  }, 0);
+});
+
+test( 'is getLang returning a string', function(assert) {
+  var expected = 'HTML'
+  var fakeRepos = [{language: 'HTML'}, {language: 'CSS'}]
+  assert.equal(getLang(fakeRepos), 'HTML CSS');
+});
